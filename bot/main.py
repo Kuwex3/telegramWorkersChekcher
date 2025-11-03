5db8aea5a8c8d6a7977d3c25718f11c788151cb4
Kuwex <vizinovskiy@gmail.com>
2025-11-04 01:14:04 +0300

    Start msg with btn

--- bot/main.py
+++ bot/main.py
@@ -4,6 +4,8 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from aiogram.enums import ParseMode
 
+from bot.sources.keyboards import kb1
+
 import os
 from dotenv import load_dotenv
 
@@ -17,7 +19,7 @@ dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def start_handler(message: types.message):
-    await message.answer(f"{FM}", parse_mode=ParseMode.HTML)
+    await message.answer(f"{FM}", parse_mode=ParseMode.HTML, reply_markup=kb1)
 
 async def main():
     await dp.start_polling(bot)
--- bot/sources/keyboards.py
+++ bot/sources/keyboards.py
@@ -2,4 +2,4 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 btn1 = InlineKeyboardButton(text="Создать", callback_data="Create")
 
-kb1 = InlineKeyboardMarkup([btn1])
+kb1 = InlineKeyboardMarkup(inline_keyboard=[[btn1]])
