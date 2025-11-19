90f0abff4a5c81434e43c48594a6ebefa52a0fd6
Kuwex <vizinovskiy@gmail.com>
2025-11-19 12:20:10 +0300

    add a checker to has company in register

--- dataBase/midlewares/registerCompany.py
+++ dataBase/midlewares/registerCompany.py
@@ -2,4 +2,5 @@ from dataBase.config import curs, conn
 
 def regCompany(mass):
     curs.execute("INSERT INTO companies (name, ownername, ownertgid) VALUES (%s, %s, %s)", (mass[0], mass[1], mass[2]))
+    curs.execute("INSERT INTO tg_users (name, isowner, tgid) VALUES (%s, %s, %s)", (mass[1], True, mass[2]))
     conn.commit()
