c2a1c86f0cd30d33ed034bde2d558741bb29f5a4
Kuwex <vizinovskiy@gmail.com>
2025-11-22 23:42:23 +0300

    edit first admin message

--- bot/handlers/adminMenu.py
+++ bot/handlers/adminMenu.py
@@ -9,9 +9,9 @@ import dotenv
 dotenv.load_dotenv()
 
 password = os.getenv("ADMINPASS")
-
+AdminFirstMessage = os.getenv("ADMIN_FM")
 router = Router()
 
 @router.message(Command(f"/wakk{password}"))
 async def admin_panel(message: types.Message):
-    await message.answer("Админ Панель!", reply_markup=adminKeyboard)
+    await message.answer(AdminFirstMessage, reply_markup=adminKeyboard, parse_mode="HTML")
