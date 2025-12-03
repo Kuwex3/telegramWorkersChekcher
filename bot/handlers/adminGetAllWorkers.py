cd63ce92101ba5ca865442e26746617f3617a1b7
KuweK <vizinovskiy@gmail.com>
2025-12-03 20:16:07 +0300

    add formated all workers out

--- bot/handlers/adminGetAllWorkers.py
+++ bot/handlers/adminGetAllWorkers.py
@@ -9,4 +9,9 @@ router = Router()
 @router.callback_query(F.data == "CheckAllWorkers")
 async def getAllWorkers(callback: types.CallbackQuery):
     data = getAllWorkersDef()
-    await callback.message.edit_text(f"{data}", parse_mode="HTML", reply_markup=adminkb)
+    formated_massive = []
+    for i in range(len(data)):
+        formated_massive.append(f"Работник: <b>{data[i][0]}</b>\nКомпания: <b>{data[i][1]}</b>")
+        formated_massive.append("-----------------------------------------------------")
+    formated_message = "\n".join(formated_massive)
+    await callback.message.edit_text(f"Все работники:\n-----------------------------------------------------\n{formated_message}", parse_mode="HTML", reply_markup=adminkb)
