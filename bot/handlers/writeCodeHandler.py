530c6f2a81b9b120c06bcff5e26e648acb753040
KuweK <vizinovskiy@gmail.com>
2025-12-10 18:29:02 +0300

    edit message text

--- bot/handlers/writeCodeHandler.py
+++ bot/handlers/writeCodeHandler.py
@@ -16,4 +16,4 @@ async def enterCodeHandler(message: types.Message, state: FSMContext):
     data = await state.get_data()
     msg = data.get("msgForEdit")
     if result[0] != "not has company":
-        await msg.edit_text(f"Вы действительно хотите присоединится в компанию {result[0]}?", parse_mode="HTML", reply_markup=backToMainMenuBeyboard)
+        await msg.edit_text(f"Вы действительно хотите присоединится к компании {result[0]}?", parse_mode="HTML", reply_markup=backToMainMenuBeyboard)
