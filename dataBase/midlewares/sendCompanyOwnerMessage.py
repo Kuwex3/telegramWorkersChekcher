20e4e73792b146a9ec93e726462868f16fe0adbb
Kuwex <vizinovskiy@gmail.com>
2025-12-21 19:55:48 +0300

    edit message text

--- dataBase/midlewares/sendCompanyOwnerMessage.py
+++ dataBase/midlewares/sendCompanyOwnerMessage.py
@@ -1,4 +1,4 @@
 from bot.sources.keyboards import backToMainMenuBeyboard as kb
 
 async def sendOwnerMessage(tgid, bot):
-    await bot.send_message(chat_id=tgid, text="Lorem", reply_markup=kb)
+    await bot.send_message(chat_id=tgid, text="К вашей компании хочет присоединится человек.", reply_markup=kb)
