a4d2ee2a557bd3b9ecbd22cbe2ca8d75e3d48da6
Kuwex <vizinovskiy@gmail.com>
2026-01-02 23:41:48 +0300

    edit message text

--- dataBase/midlewares/sendCompanyOwnerMessage.py
+++ dataBase/midlewares/sendCompanyOwnerMessage.py
@@ -3,8 +3,8 @@ from otherScripts.keyboardBuilderForAccept import getKeyboard
 async def sendOwnerMessage(tgid, bot, firstname, lastname, usertag, userid, company):
     kb = getKeyboard(userid, company)
     if lastname == None and userid == None:
-        await bot.send_message(chat_id=tgid, text=f"К вашей компании хочет присоединится {firstname}.", reply_markup=kb, parse_mode = "HTML")
+        await bot.send_message(chat_id=tgid, text=f"К вашей компании хочет присоединится {firstname}", reply_markup=kb, parse_mode = "HTML")
     elif lastname == None:
-        await bot.send_message(chat_id=tgid, text=f"К вашей компании хочет присоединится {firstname} <b>@{usertag}</b>.", reply_markup=kb, parse_mode = "HTML")
+        await bot.send_message(chat_id=tgid, text=f"К вашей компании хочет присоединится {firstname} <b>@{usertag}</b>", reply_markup=kb, parse_mode = "HTML")
     else:
-        await bot.send_message(chat_id=tgid, text=f"К вашей компании хочет присоединится {firstname} {lastname} <b>@{usertag}</b>.", reply_markup=kb, parse_mode = "HTML")
+        await bot.send_message(chat_id=tgid, text=f"К вашей компании хочет присоединится {firstname} {lastname} <b>@{usertag}</b>", reply_markup=kb, parse_mode = "HTML")
